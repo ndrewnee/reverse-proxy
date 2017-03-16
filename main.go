d5b0826d2f071fa080058b8501835e4ca4f7037c
ndrewnee <ndrewnee@gmail.com>
2017-03-16 19:28:36 +0500

    - Added rule about host to message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 // This utility start reverse proxy server that can search
 // string in response body of host and replace it to the given string
 // Usage: reverse-proxy <host> <search> <replace>
+// <host> must contain "http" or "https"
 package main
 
 import (
@@ -13,7 +14,7 @@ import (
 
 func main() {
 	if len(os.Args) < 4 {
-		fmt.Println("Usage: reverse-proxy <host> <search> <replace>")
+		fmt.Println("Usage: reverse-proxy <host> <search> <replace>.\n<host> must contain 'http' or 'https'")
 		return
 	}
 
